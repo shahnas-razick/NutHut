73b08d68073b99d59f3a944d981e8f6b73045dcf
Shahnas-razick <shahnasrazick@gmail.com>
2025-02-20 11:30:35 +0530

    Add files via upload

---
+++ 3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+float payrate;
+int hours;
+printf("Enter total work hours: ");
+scanf ("%d",&hours);
+printf ("Enter payrate: ");
+scanf ("%f", &payrate);
+printf("your pay rate:%.2f, worked hours:%d",payrate);
+}
+
+
+
